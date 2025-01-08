4052446c501965066b5ee90e894b90b6ec43e801
Raresica03 <padurerares131@gmail.com>
2025-01-08 20:05:13 +0200

    added files after reinstall

--- backend/Application.API/Application.API/obj/Debug/net8.0/Application.API.AssemblyInfo.cs
+++ backend/Application.API/Application.API/obj/Debug/net8.0/Application.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca8f659638020d6227e4a4f400583e1549b319d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c9793303a4b54984af39a3c3ccd33bf9ce38f62")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
